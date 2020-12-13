d80b654a3a0608780098d5f06167b495306f0170
ug00031 <ug00031@surrey.ac.uk>
2020-12-13 20:19:31 +0000

    remove forgery

--- app/controllers/api/user_token_controller.rb
+++ app/controllers/api/user_token_controller.rb
@@ -1,6 +1,6 @@
 module Api
     class UserTokenController < Knock::AuthTokenController
         skip_before_action :verify_authenticity_token
-        protect_from_forgery with: :null_session
+        # protect_from_forgery with: :null_session
     end
 end
