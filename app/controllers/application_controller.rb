b67a7bc50551a3f395aaf12dcf3870945e2170fe
ug00031 <ug00031@surrey.ac.uk>
2020-12-13 20:19:54 +0000

    remove

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     include Knock::Authenticable
-    protect_from_forgery with: :null_session
+    # protect_from_forgery with: :null_session
 
     def auth_user
         authenticate_for API::user;
